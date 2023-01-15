55382576c554b778ce1c8aaaefc391879d7f4937
Kevin Mamaqi <kevinmamaqi@gmail.com>
2023-01-15 15:24:37 +0100

    segunda-revision

--- src/components/atoms/Label.jsx
+++ src/components/atoms/Label.jsx
@@ -1,5 +1,5 @@
-export default function Label({children, id}){
-    return (
-        <label htmlFor={id}>{children}</label>
-    )
+export default function Label({ children, id }) {
+  return <label htmlFor={id}>{children}</label>;
 }
+
+// Añade validación por prop-types.
